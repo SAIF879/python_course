86b904273cab30cfb1bd0fbd89468c7bea2312ed
SAIF879 <saifahmed9308@gmail.com>
2023-08-18 15:34:27 +0530

    learning

---
+++ .idea/main/lists.py
@@ -0,0 +1,8 @@
+my_list = [1, 2, 3, 'c', "saif" , [1,2,4] , (6,6,6,6)]
+second_list = [4,4,4 , "ahmed" , "array"]
+my_list.extend(second_list)
+third_list = my_list[3:]
+
+my_tuple = (1,2,3, "saif", "fail" , [1,2,3], (4,5,6))
+
+print(my_tuple)
